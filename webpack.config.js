5cc36caf79733ba1d54644e9b87222b19a219948
Vincent <v.rell_home@outlook.com>
2019-03-01 11:45:23 +0100

    prepared files for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "dist/")
+    path: path.resolve(__dirname, "docs/")
   },
   devServer: {
     contentBase:'dist'
